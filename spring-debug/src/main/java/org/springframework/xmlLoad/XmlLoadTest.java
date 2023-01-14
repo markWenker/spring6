3c23e00d25700269828ffb5634b11329fb824663
mark.wang <834031172@qq.com>
2023-01-14 21:27:06 +0800

    feat: xml load
    1. add class to debug spring source code startup process
    2. load simple xml process and simple bean

---
+++ spring-debug/src/main/java/org/springframework/xmlLoad/XmlLoadTest.java
@@ -0,0 +1,19 @@
+package org.springframework.xmlLoad;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.entity.Person;
+
+/**
+ * debug spring启动以及加载简单xml和简单bean过程
+ *
+ * @author wangmingxing
+ */
+public class XmlLoadTest {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("xmlLoad/xmlLoad.xml");
+		Person person = (Person) ac.getBean("person");
+		System.out.println("name type: " + person);
+		Person bean = ac.getBean(Person.class);
+		System.out.println("class type: " + bean);
+	}
+}
