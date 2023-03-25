7aedbe349b48bf74eee982e7e2fa7f3e23e7b6dc
mark.wang <834031172@qq.com>
2023-03-25 11:36:32 +0800

    docs: invokeBeanFactoryPostProcessors
    invokeBeanFactoryPostProcessors notes

---
+++ spring-debug/src/main/java/org/springframework/selfbdrpp/MyBeanFactoryPostProcessor.java
@@ -0,0 +1,21 @@
+package org.springframework.selfbdrpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+/**
+ * @author mark.wang
+ * @since 2023/3/25
+ */
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+	@Override
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+		BeanDefinition person = beanFactory.getBeanDefinition("Person");
+		//此处可以对对应的BeanDefinition进行相关设置修改操作
+		person.setLazyInit(false);
+		System.out.println("自定义的BeanFactoryPostProcessor");
+	}
+}
