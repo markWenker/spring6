a020ef0cf926e913a775c3ca5baa41bf0cf99e37
mark wang <834031172@qq.com>
2023-07-23 12:09:26 +0800

    fix: 传入错误的bean名称问题

--- spring-debug/src/main/java/org/springframework/selfbdrpp/MyBeanFactoryPostProcessor.java
+++ spring-debug/src/main/java/org/springframework/selfbdrpp/MyBeanFactoryPostProcessor.java
@@ -13,7 +13,7 @@ public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
 
 	@Override
 	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-		BeanDefinition person = beanFactory.getBeanDefinition("Person");
+		BeanDefinition person = beanFactory.getBeanDefinition("person");
 		//此处可以对对应的BeanDefinition进行相关设置修改操作
 		person.setLazyInit(false);
 		System.out.println("自定义的BeanFactoryPostProcessor");
